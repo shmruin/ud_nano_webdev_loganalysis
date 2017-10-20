4de64741b4a0625ba80da64c69f2d0c1d1c836a7
ruin09 <meme_hm@naver.com>
2017-10-20 17:34:00 +0900

    * Resummit Ver 'Log_Analysis' project
    Adjusting Reviewed points

--- newsdata.py
+++ newsdata.py
@@ -1,51 +1,67 @@
+#!/usr/bin/env python
 import psycopg2
 
 DBNAME = "news"
 
+def connect(database_name):
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print("Unable to connect to database")
+        # THEN perhaps exit the program
+        sys.exit(1) # The easier method
+        # OR perhaps throw an error
+        raise e
+        # If you choose to raise an exception,
+        # It will need to be caught by the whoever called this function
 
-def get_most_popular_3articles():
-    """What are the most popular 3 articles of all time?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select A.title, count(B.path) as views from articles as A, log as B where \
-    B.path like concat('%', A.slug) group by A.title order by views desc \
-    limit 3;")
-    return c.fetchall()
+def get_query_results(query):
+    # connect to database, grab cursor
+    db, c = connect(DBNAME)
+    # execute
+    c.execute(query)
+    # commit
+    results = c.fetchall()
+    # close
     db.close()
+    # return results
+    return results
 
+if __name__ == '__main__':
 
-def get_most_popular_authors():
+    """What are the most popular 3 articles of all time?"""
+    query1 = "select A.title, count(B.path) as views from articles as A, log as B where \
+    B.path like concat('%', A.slug) group by A.title order by views desc \
+    limit 3;"
+
     """Who are the most popular article authors of all time?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select A.name, sum(B.views) as author_views from (select B.id, \
+    query2 = "select A.name, sum(B.views) as author_views from (select B.id, \
     B.name, A.title from articles as A, authors as B where A.author = B.id) \
     as A, (select A.title, count(B.path) as views from articles as A, log \
     as B where B.path like concat('%', A.slug) group by A.title order by \
     views desc) as B where A.title = B.title group by A.name order by \
-    author_views desc;")
-    return c.fetchall()
-    db.close()
+    author_views desc;"
 
-
-def get_day_more_than_1percent_req_error():
     """On which days did more than 1% of requests lead to errors?"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select log_date, round((times * 100 / sum(times) over()), 1) as percentage \
+    query3 = "select log_date, round((times * 100 / sum(times) over()), 1) as percentage \
     from (with error_nums as (select A.time::date as log_date, \
     count(A.status) as times from log as A where \
     A.status like '404%' group by log_date) select log_date, \
     times from error_nums where times > (select avg(times) from error_nums) \
-    order by log_date) as K;")
-    return c.fetchall()
-    db.close()
+    order by log_date) as K;"
 
-
-if __name__ == '__main__':
     print("1. What are the most popular three articles of all time?")
-    print(get_most_popular_3articles())
+    results = get_query_results(query1)
+    for el in results:
+        print("\"" + el[0] + "\"" + " - " + str(el[1]) + " views")
     print("2. Who are the most popular article authors of all time?")
-    print(get_most_popular_authors())
-    print("3. On which days did more than 1%% of requests lead to errors?")
-    print(get_day_more_than_1percent_req_error())
+    results = get_query_results(query2)
+    for el in results:
+        print("\"" + el[0] + "\"" + " - " + str(el[1]) + " views")
+    print("3. On which days did more than 1% of requests lead to errors?")
+    results = get_query_results(query3)
+    for el in results:
+        print(el[0].strftime("%b %d, %Y") + " - " + str(el[1]) + "% errors")
